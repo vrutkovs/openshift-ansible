6778dbffe57b40ae20180eb2fc9a910370f5db09
Vadim Rutkovsky <roignac@gmail.com>
2018-11-16 18:59:30 +0100

    GCP: remove bootstrap node once install is complete

---
+++ roles/openshift_gcp/templates/remove_bootstrap.j2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+dns_zone="{{ dns_managed_zone | default(openshift_gcp_prefix + 'managed-zone') }}"
+# configure DNS
+(
+# Retry DNS changes until they succeed since this may be a shared resource
+while true; do
+    dns="${TMPDIR:-/tmp}/dns.yaml"
+    dns_transaction="${TMPDIR:-/tmp}/dns.transaction"
+    rm -f $dns
+    rm -f $dns_transaction
+    gcloud --project "{{ openshift_gcp_project }}" dns record-sets transaction --transaction-file=$dns_transaction start -z "${dns_zone}"
+    gcloud dns record-sets export --project "{{ openshift_gcp_project }}" -z "${dns_zone}" --zone-file-format "${dns}"
+
+    # Fetch API record to get a list of masters + bootstrap node
+    public_ip_output=($(grep -F -e '{{ openshift_master_cluster_public_hostname }}.' "${dns}" | awk '{ print $5 }')) || public_ip_output=""
+
+    for index in "${!public_ip_output[@]}"; do
+        if [ ${index} -eq 0 ]; then
+            # Remove first record - its an address of bootstrap node
+            grep -F -e "{{ openshift_master_cluster_public_hostname }}." "${dns}" | awk '{ print "--name", $1, "--ttl", $2, "--type", $4, $5; }' | head -n1 > "${dns}.input" || true
+            break
+        fi
+    done
+    if [ -s "${dns}.input" ]; then
+        cat "${dns}.input"
+        cat "${dns}.input" | xargs -L1 gcloud --project "{{ openshift_gcp_project }}" dns record-sets transaction --transaction-file="${dns_transaction}" remove -z "${dns_zone}"
+        cat "${dns_transaction}"
+        # Commit all DNS changes, retrying if preconditions are not met
+        if ! out="$( gcloud --project "{{ openshift_gcp_project }}" dns record-sets transaction --transaction-file=$dns_transaction execute -z "${dns_zone}" 2>&1 )"; then
+            rc=$?
+            if [[ "${out}" == *"HTTPError 412: Precondition not met"* ]]; then
+                continue
+            fi
+            exit $rc
+        fi
+    fi
+    rm "${dns}.input"
+    break
+done
+) &
+
+for i in `jobs -p`; do wait $i; done
